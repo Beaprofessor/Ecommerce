03804f1ae2fae44b036e47678a3a3ecd08fa626b
Beaprofessor <professornberlin2122@gmail.com>
2024-06-21 23:44:52 +0530

    Made the dark mode

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const darkModeSwitch = document.getElementById('darkModeSwitch');
+    const body = document.body;
+
+    darkModeSwitch.addEventListener('change', function () {
+      body.classList.toggle('dark-mode');
+    });
+  });
